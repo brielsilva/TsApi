5ee7be6c3215210532085e3c2d59d1f7f62c4375
Gabriel Costa <gabrielcostasilva100@gmail.com>
2022-03-04 10:40:46 -0300

    config

--- test/functional/forecast.test.js
+++ test/functional/forecast.test.js
@@ -4,7 +4,7 @@ describe('Beach forecast functional tests',() => {
     it('Should return a forecast with just a few times', async () => {
         const {body, status} = await supertest(app).get('/forecast');
         expect(status).toBe(200);
-        expect(body)toBe.([
+        expect(body).toBe([
             {
                 'name':'yes'
             },
